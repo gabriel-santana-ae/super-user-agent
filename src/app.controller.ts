a3f3d51e49d5d6cb9d6d113b829d07064101423e
Gabriel V. Santana <gabriel.santana@agileengine.com>
2023-06-17 00:45:34 -0300

    fix: remove type

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Req } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get('test')
-  async test(@Req() request: Request): Promise<any> {
+  async test(@Req() request): Promise<any> {
     const userAgent = request.headers['user-agent'];
     console.log('///////////////////////////////////');
     console.log('userAgent');
